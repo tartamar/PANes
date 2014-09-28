365e3ad220fd22687a82e0bacfbff3a4c16a046b
tartamar <martin.tartarelli@gmail.com>
2014-09-28 18:55:54 -0300

    First Commint

---
+++ Form1.Designer.vb
@@ -0,0 +1,131 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.ListBox1 = New System.Windows.Forms.ListBox
+        Me.Button_Search = New System.Windows.Forms.Button
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.Label2 = New System.Windows.Forms.Label
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(12, 35)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(407, 20)
+        Me.TextBox1.TabIndex = 1
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(12, 135)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(548, 290)
+        Me.ListBox1.TabIndex = 2
+        '
+        'Button_Search
+        '
+        Me.Button_Search.Location = New System.Drawing.Point(435, 19)
+        Me.Button_Search.Name = "Button_Search"
+        Me.Button_Search.Size = New System.Drawing.Size(125, 75)
+        Me.Button_Search.TabIndex = 3
+        Me.Button_Search.Text = "Search"
+        Me.Button_Search.UseVisualStyleBackColor = True
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 74)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(407, 20)
+        Me.TextBox2.TabIndex = 4
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Location = New System.Drawing.Point(6, 109)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(560, 329)
+        Me.GroupBox1.TabIndex = 5
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Search PAN Result"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 19)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Source PATH"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 58)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Destination PATH"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Location = New System.Drawing.Point(6, 3)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(560, 100)
+        Me.GroupBox2.TabIndex = 0
+        Me.GroupBox2.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(570, 442)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.Button_Search)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "PANes"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+    Friend WithEvents Button_Search As System.Windows.Forms.Button
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+
+End Class
---
+++ Form1.vb
@@ -0,0 +1,70 @@
+﻿Imports VBScript_RegExp_55
+
+Public Class Form1
+
+    'función para ejecutar la expesión regular
+    Function Extraer_direccion_Email(ByVal PathOrigen As String, _
+                                ByVal pathDestino As String, _
+                                ByVal Cadena_Expresion As String) As Boolean
+
+        Dim obj_Expresion As RegExp, Match, Matches
+
+
+        Dim fso As Object, fil As Object, contenido As String, sal As Object
+
+        'Nuevo objeto para crear leer y los archivos con Fso
+        fso = CreateObject("Scripting.FileSystemObject")
+
+        'Lee el archivo de origen y lo almacena en contenido
+        fil = fso.OpenTextFile(PathOrigen, 1)
+
+        contenido = fil.ReadAll
+
+        'Crea un archivo para guardar las direcciones de email
+        sal = fso.CreateTextFile(pathDestino, True)
+
+        'Nuevo objeto de tipo RegExp para poder utilizar las expresiones regulares
+        obj_Expresion = New RegExp
+
+        'Se le pasa la expresion regular a la propiedad Pattern
+        obj_Expresion.Pattern = Cadena_Expresion
+
+        obj_Expresion.IgnoreCase = True
+
+        obj_Expresion.Global = True
+
+        ' Método Execute para pasarle el contenido del archivo a Evaluar
+        Matches = obj_Expresion.Execute(contenido)
+
+        ' recorre en la colección las direcciones y las escribe en el archivo txt de salida y las agrega al control List
+
+        For Each Match In Matches
+            sal.WriteLine(Match.Value)
+            ListBox1.Items.Add(Match.Value)
+        Next
+
+        'Cierra los archivos
+        sal.Close()
+        fil.Close()
+
+        'Fin
+        MsgBox(" Finalizado ", vbInformation)
+        Extraer_direccion_Email = True
+
+        Exit Function
+
+        'Error
+errSub:
+        MsgBox(Err.Description, vbCritical)
+        Extraer_direccion_Email = False
+
+    End Function
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button_Search.Click
+
+        'Basic credit card
+        'Call Extraer_direccion_Email(TextBox1.Text, TextBox2.Text, "^(\d{4}[- ]){3}\d{4}|\d{16}$")
+        Call Extraer_direccion_Email(TextBox1.Text, TextBox2.Text, "(\d[\s|-]?){12,15}\d")
+
+    End Sub
+End Class
